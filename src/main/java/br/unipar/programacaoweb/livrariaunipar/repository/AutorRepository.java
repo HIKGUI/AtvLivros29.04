c5be17af5a4a2102dd20aa2019ccd84be722e41a
Guilherme H. P. Cravo <guilherme.h.p.cravo@gmail.com>
2025-05-04 22:25:00 -0300

    ATV 04.05

--- src/main/java/br/unipar/programacaoweb/livrariaunipar/repository/AutorRepository.java
+++ src/main/java/br/unipar/programacaoweb/livrariaunipar/repository/AutorRepository.java
@@ -1,5 +1,6 @@
 package br.unipar.programacaoweb.livrariaunipar.repository;
 
+import br.unipar.programacaoweb.livrariaunipar.model.Autor;
 import br.unipar.programacaoweb.livrariaunipar.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,9 +12,9 @@ import java.util.List;
 @Repository
 public interface AutorRepository extends JpaRepository<Livro, Long> {
 
-    List<Livro> findLivroByTituloContainingIgnoreCase(String titulo);
+    List<Autor> findLivroByTituloContainingIgnoreCase(String titulo);
 
-    List<Livro> findLivroByGeneroContainingIgnoreCase(String genero);
+    List<Autor> findLivroByGeneroContainingIgnoreCase(String genero);
 
     @Query("SELECT t FROM Livro t WHERE t.titulo = :genero AND t.numeroPaginas >= :numeroPaginas")
     List<Livro> findLivroBygeneroeNumerPagina(@Param("genero") String genero,
